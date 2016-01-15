1ed5c6e2ec205e2a210ea84aeb82003a76b7f6c7
Quentin Machard <quentin.machard@gmail.com>
2016-01-15 14:24:30 +0100

    Initialisation du jeu

--- app/controllers/Game.js
+++ app/controllers/Game.js
@@ -1,79 +1,39 @@
 "use strict";
 
+import Element from "../models/Element.js";
+import Board from "../models/Board.js";
+
 class Game {
 	constructor() {
-		this.board = new Board(20, this);
-		this.snake = new Snake(this);
-		this.food = new Food(this);
-		this.score = new Score(this);
-		this.frames = null;
+		this.element = new Element();
+		this.board = new Board();
 
-		this.speed = 70;
+		this.domElement = document.getElementById('game');
+
+		this.frames = null
 	}
 
 	init() {
+		this.element = new Element();
+		this.
+
+		this.element.render();
+
+		this.startInterval();
 		this.controls();
+	}
 
+	startInterval() {
 		var self = this;
-		document.getElementById('btnStart').addEventListener('click', function() { self.start(); }, false);
+		this.frames = setInterval(function() { self.frame(); }, 500);
+	}
 
-		this.food.changePosition();
-		this.food.view.render();
+	stopInterval() {
+		clearInterval(this.frames);
 	}
 
 	frame() {
-		this.snake.move();
-		this.snake.view.render();
-
-		// Détecter les bords du jeu
-		if(this.snake.position.x < 0 || this.snake.position.x > this.board.width / this.board.sizeCase - 1 || this.snake.position.y < 0 || this.snake.position.y > this.board.height / this.board.sizeCase - 1) {
-			this.gameOver();
-		}
-
-		// Manger l'aliment
-		if(this.snake.position.x == this.food.position.x && this.snake.position.y == this.food.position.y) {
-			this.snake.addElement();
-
-			this.food.changePosition();
-			this.food.view.render();
-
-			this.score.increment();
-			this.score.view.render();
-		}
-
-		// Collisions
-		for(var i=1, element; element = this.snake.elements[i]; i++) {
-			if (this.snake.position.x == element.position.x && this.snake.position.y == element.position.y) {
-				this.gameOver();
-			}
-		}
-	}
-
-	start() {
-		this.snake.reset();
-
-		this.food.changePosition();
-		this.food.view.render();
-
-		this.score.reset();
-		this.score.view.render();
-
-		document.getElementById('splash').style.display = 'none';
-
-		var self = this;
-		if(this.frames == null) {
-			this.frames = setInterval(function() { self.frame() }, this.speed);
-		}
-	}
-
-	gameOver() {
-		clearInterval(this.frames);
-		this.frames = null;
-
-		this.score.saveHighScore();
-		this.score.view.renderTable();
-
-		document.getElementById('splash').style.display = 'table';
+		this.element.moveDown();
 	}
 
 	controls() {
@@ -87,16 +47,19 @@ class Game {
 						self.start();
 						break;
 					case 37: // Gauche
-						self.snake.moveLeft();
+						self.element.moveLeft();
 						break;
 					case 38: // Haut
-						self.snake.moveUp();
+						//self.stopInterval();
+						self.element.rotate();
+						//self.startInterval();
 						break;
 					case 39: // Droite
-						self.snake.moveRight();
+
+						self.element.moveRight();
 						break;
 					case 40: // Bas
-						self.snake.moveDown();
+						self.element.moveDown();
 						break;
 				}
 			}
